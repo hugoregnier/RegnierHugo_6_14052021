dcbff9dab57f4c570cb3ffd6883a4d7283e5b3f2
Hugo REGNIER <hma.regnier@orange.fr>
2021-05-14 15:43:47 +0200

    add all routes / create schema

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const Product = require('./models/Products');
 
 const app = express();
 
@@ -19,23 +20,37 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-    console.log('Requête reçue !');
-    next();
-});
-
-app.use((req, res, next) => {
-    res.status(201);
-    next();
-});
-
-app.use((req, res, next) => {
-    res.json({ message: 'Votre requête a bien été reçue !' });
-    next();
-});
-
-app.use((req, res, next) => {
-    console.log('Réponse envoyée avec succès !');
-});
-
-module.exports = app;
+app.post('/api/products', (req, res, next) => {
+    const product = new Product({
+      ...req.body
+    });
+    product.save()
+      .then(() => res.status(201).json({ product }))
+      .catch(error => res.status(400).json({ error }));
+  });
+   
+  app.get('/api/products/:id', (req, res, next) => {
+    Product.findOne({ _id: req.params.id })
+      .then(product => res.status(200).json({ product }))
+      .catch(error => res.status(404).json({ error }));
+  });
+   
+  app.put('/api/products/:id', (req, res, next) => {
+    Product.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+      .catch(error => res.status(400).json({ error }));
+  });
+   
+  app.delete('/api/products/:id', (req, res, next) => {
+    Product.deleteOne({ _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Deleted !'}))
+      .catch(error => res.status(400).json({ error }));
+  });
+   
+  app.get('/api/products', (req, res, next) => {
+      Product.find()
+        .then(products => res.status(200).json({ products }))
+        .catch(error => res.status(400).json({ error }));
+  });
+  
+  module.exports = app;
