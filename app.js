5f1dbc9ad4b288b47738f829b1e3a8d9f0e762e6
Hugo REGNIER <hma.regnier@orange.fr>
2021-05-14 15:37:36 +0200

    add bodyparser / add mongoose

--- app.js
+++ app.js
@@ -1,31 +1,41 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://hugoregnier:hugo21@cluster0.sumrx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-  });
+});
+
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  console.log('Requête reçue !');
-  next();
+    console.log('Requête reçue !');
+    next();
 });
 
 app.use((req, res, next) => {
-  res.status(201);
-  next();
+    res.status(201);
+    next();
 });
 
 app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
+    res.json({ message: 'Votre requête a bien été reçue !' });
+    next();
 });
 
 app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
+    console.log('Réponse envoyée avec succès !');
 });
 
 module.exports = app;
