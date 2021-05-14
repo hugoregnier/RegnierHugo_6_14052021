33dc006ac3443e295d64188b926939bdc6f09b07
Hugo REGNIER <hma.regnier@orange.fr>
2021-05-14 17:22:44 +0200

    add user

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const saucesRoutes = require('./routes/sauces')
+const userRoutes = require('./routes/User')
 
 const app = express();
 
@@ -22,5 +23,6 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 app.use('/api/sauces', saucesRoutes);
+app.use('/api/auth', userRoutes);
   
   module.exports = app;
--- models/Sauce.js
+++ models/Sauce.js
@@ -2,9 +2,15 @@ const mongoose = require('mongoose');
 
 const sauceSchema = mongoose.Schema({
   name: { type: String, required: true },
+  manufacturer: { type: String, required: true },
   description: { type: String, required: true },
-  price: { type: Number, required: true },
-  inStock: { type: Boolean, required: true },
+  mainPepper: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  heat: { type: Number, required: true },
+  likes: { type: Number, required: true },
+  dislikes: { type: Number, required: true },
+  usersLiked: { type: String = [], required: true },
+  usersDisliked: { type: String = [], required: true },
 });
 
 module.exports = mongoose.model('Sauce', sauceSchema);
