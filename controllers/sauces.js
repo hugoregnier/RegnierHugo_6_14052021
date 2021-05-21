575fd423f0d2be58514f4387b496300f9bf8c525
Hugo REGNIER <hma.regnier@orange.fr>
2021-05-21 16:11:58 +0200

    add multer , modif image an delete

--- controllers/sauces.js
+++ controllers/sauces.js
@@ -1,20 +1,28 @@
 const Sauce = require('../models/Sauce')
 
+
 exports.createSauce = (req, res, next) => {
-    console.log(req.body)
+    const sauceObject = JSON.parse(req.body.sauce);
+    delete sauceObject._id;
     const sauce = new Sauce({
-        ...req.body
+      ...sauceObject,
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
     sauce.save()
-        .then(() => res.status(201).json({ sauce }))
-        .catch(error => res.status(400).json({ error }));
-}
+      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+      .catch(error => res.status(400).json({ error }));
+  };
 
-exports.modifySauce = (req, res, next) => {
-    Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Objet modifié !' }))
-        .catch(error => res.status(400).json({ error }));
-}
+  exports.modifySauce = (req, res, next) => {
+    const sauceObject = req.file ?
+      {
+        ...JSON.parse(req.body.sauce),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+      } : { ...req.body };
+    Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+      .catch(error => res.status(400).json({ error }));
+  };
 
 exports.deleteSauce = (req, res, next) => {
     Sauce.deleteOne({ _id: req.params.id })
--- routes/sauces.js
+++ routes/sauces.js
@@ -3,11 +3,12 @@ const router = express.Router();
 
 const saucesCtrl = require('../controllers/sauces')
 const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config')
 
-router.post('/', auth,saucesCtrl.createSauce);
-router.get('/:id', auth,saucesCtrl.getOneSauce);
-router.put('/:id', auth,saucesCtrl.modifySauce);
-router.delete('/:id', auth,saucesCtrl.deleteSauce);
-router.get('/', auth,saucesCtrl.getAllSauce);
+router.post('/', auth,multer, saucesCtrl.createSauce);
+router.get('/:id', auth, saucesCtrl.getOneSauce);
+router.put('/:id', auth, multer, saucesCtrl.modifySauce);
+router.delete('/:id', auth, saucesCtrl.deleteSauce);
+router.get('/', auth, saucesCtrl.getAllSauce);
 
 module.exports = router;
